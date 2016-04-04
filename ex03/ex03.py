ae76f03b5cbbf638bab8c7635b58685cd001029c
Karlos <nomail@mail.com>
2016-04-04 23:18:34 +0200

    Exercise 03 upload (without study drills)

---
+++ ex03/ex03.py
@@ -0,0 +1,20 @@
+print("I will now count my chickens")
+
+# Printing chicken counts in lines below
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+# Counting & printing egg count
+print("Now I will count the eggs:")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)  # In "what you should see" result is 7, but Python3 gives float for 1/4
+
+# More fun with operators
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+print("Oh, that's why it's false")
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+

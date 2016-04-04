fc4354b0f6cc5a422ccd68da17c1f4229c468bcf
Karlos <nomail@mail.com>
2016-04-04 23:02:05 +0200

    Exercise 02 upload (without study drills)

---
+++ ex02/ex02.py
@@ -0,0 +1,15 @@
+#Comment here, comment there
+
+print("I could have code like this.") # and comment is ignored
+
+# Not working print:
+# print("This won't run")
+
+'''
+Why
+not
+have
+a
+multilne comment?
+'''
+print("This will run.")
